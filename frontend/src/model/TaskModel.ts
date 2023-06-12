35b456a7cd92a1af57cad59c057f656c43258201
svenjaklenke <svenja.klenke@gmx.de>
2023-06-12 10:16:33 +0200

    Add model task

---
+++ frontend/src/model/TaskModel.ts
@@ -0,0 +1,10 @@
+export type Task = {
+    id: string,
+    creator: string,
+    category: string,
+    name: string,
+    createDate: string,
+    deadline: string,
+    amoundOfPeople: number,
+    text: string,
+}
