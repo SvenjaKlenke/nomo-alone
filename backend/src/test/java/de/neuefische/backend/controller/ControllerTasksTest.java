8e5ff71312e2c322ec841d992e4635140ff912ae
svenjaklenke <svenja.klenke@gmx.de>
2023-06-16 11:29:46 +0200

    add controllertest for deleteByID

--- backend/src/test/java/de/neuefische/backend/controller/ControllerTasksTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/ControllerTasksTest.java
@@ -1,7 +1,7 @@
 package de.neuefische.backend.controller;
 
-import de.neuefische.backend.repository.RepoTasks;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.backend.model.TaskModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -58,5 +59,37 @@ class ControllerTasksTest {
                                   "text": "Test tester Test"
                         }
                         """)).andExpect(jsonPath("$.id").isNotEmpty());
+    }
+
+    @Test
+    @DirtiesContext
+    void deleteToDo_thenReturn200OK_andReturnEmptyList() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/tasks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                                                            
+                                                  "creator": "Svenja",
+                                                  "name":"Test1",
+                                                  "category":"PLAYDATE",
+                                                  "createDate": "15.06.2023",
+                                                  "deadline": "16.06.2023",
+                                                  "amoundOfPeople": 2,
+                                                  "text": "Test tester Test"
+                                }
+                                """))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        TaskModel taskModel = objectMapper.readValue(content, TaskModel.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/tasks/" + taskModel.getId()))
+                .andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 }
