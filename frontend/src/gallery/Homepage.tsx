c1c5d7af319b8837a4877ecfae3157fb802c2e64
svenjaklenke <svenja.klenke@gmx.de>
2023-06-22 15:03:59 +0200

    add new Buttons

--- frontend/src/gallery/Homepage.tsx
+++ frontend/src/gallery/Homepage.tsx
@@ -10,8 +10,28 @@ function Homepage() {
 
     }
 
+    function clickForTasksPlaydate() {
+        navigate('/tasks/playdate');
+    }
+
+    function clickForTasksAppointment() {
+        navigate('/tasks/appointment');
+    }
+
+    function clickForTasksVisit() {
+        navigate('/tasks/visit');
+    }
+
+    function clickForTasksShopping() {
+        navigate('/tasks/shopping');
+    }
+
     return (
         <div>
+            <button className="Buttons" onClick={clickForTasksPlaydate}>Playdate</button>
+            <button className="Buttons" onClick={clickForTasksAppointment}>Appointment</button>
+            <button className="Buttons" onClick={clickForTasksVisit}>Visit</button>
+            <button className="Buttons" onClick={clickForTasksShopping}>Shopping</button>
             <button className="Buttons" onClick={clickForAllTasks}>All Tasks</button>
         </div>
     );
