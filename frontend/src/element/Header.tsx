7e5f8abe9c4c943f2afba092ca260b2f6026e90e
svenjaklenke <svenja.klenke@gmx.de>
2023-06-22 12:53:50 +0200

    add header css

--- frontend/src/element/Header.tsx
+++ frontend/src/element/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from "../Logo.png";
+import './Header.css';
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <div>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
+                <Link className="LinkHeader" to={"/"}>Home</Link>
             </header>
         </div>
     );
