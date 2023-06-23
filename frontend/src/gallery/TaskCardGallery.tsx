150d1f960391532c555815b294bcc458b6183108
svenjaklenke <svenja.klenke@gmx.de>
2023-06-23 12:56:39 +0200

    update gallery with params, to show only tasks from one category

--- frontend/src/gallery/TaskCardGallery.tsx
+++ frontend/src/gallery/TaskCardGallery.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import {TaskModel} from "../model/TaskModel";
 import TaskCard from "../cards/TaskCard";
 import './TaskCardGallery.css';
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 type Props = {
     allTasks: TaskModel[],
@@ -12,11 +12,22 @@ type Props = {
 function TaskCardGallery(props: Props) {
 
     const navigate = useNavigate();
+    const params = useParams();
+    const category: string | undefined = params.category?.toUpperCase();
+
 
     useEffect(() => {
         props.getAllTasks();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    let tasksToDisplay: TaskModel[] = [];
+
+    if (category === undefined || category === "TASKS") {
+        tasksToDisplay = props.allTasks;
+    } else if (["VISIT", "APPOINTMENT", "SHOPPING", "PLAYDATE"].includes(category)) {
+        tasksToDisplay = props.allTasks.filter(currentTask => currentTask.category === category);
+    }
 
     function clickToNewTask() {
         navigate("/new");
@@ -31,11 +42,11 @@ function TaskCardGallery(props: Props) {
                 </div>
             </div>
             <div className="Taskcardgallery">
-                {props.allTasks.length === 0 ? (
+                {tasksToDisplay.length === 0 ? (
                     <h6>There are no tasks available yet. <br/>You can add a task by clicking the "New Task" button.
                     </h6>
                 ) : (
-                    props.allTasks.map(task => <TaskCard key={task.id} task={task}/>)
+                    tasksToDisplay.map(task => <TaskCard key={task.id} task={task}/>)
                 )}
             </div>
         </div>
