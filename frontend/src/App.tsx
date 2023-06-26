cc2a803325c3393f116394e3dce39788e8559fe7
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 13:22:46 +0200

    add LoginPage in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,33 +8,45 @@ import NewTaskCard from "./cards/NewTaskCard";
 import EditTaskCard from "./cards/EditTaskCard";
 import Header from "./element/Header";
 import Homepage from "./gallery/Homepage";
+import LoginPage from "./login/LoginPage";
+import useUserModel from "./login/useUserModel";
+import ProtectedRoutes from "./login/ProtectedRoutes";
 
 function App() {
 
     const {getAllTasks, tasksList} = useTasks()
+    const {login, user} = useUserModel()
 
-  return (
-    <div className="App">
-        <Header/>
-        <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/tasks"
-                   element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks} category="TASKS"/>}/>
-            <Route path="/visit"
-                   element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks} category="VISIT"/>}/>
-            <Route path="/playdate"
-                   element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks} category="PLAYDATE"/>}/>
-            <Route path="/appointment"
-                   element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks} category="APPOINTMENT"/>}/>
-            <Route path="/shopping"
-                   element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks} category="SHOPPING"/>}/>
+    return (
+        <div className="App">
+            {user === "" || user === "anonymousUser" ? <> </> : <Header/>}
+            <Routes>
+                <Route path={"/login"} element={<LoginPage login={login}/>}/>
+                <Route element={<ProtectedRoutes user={user}/>}>
+                    <Route path="/" element={<Homepage/>}/>
+                    <Route path="/tasks"
+                           element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks}
+                                                     category="TASKS"/>}/>
+                    <Route path="/visit"
+                           element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks}
+                                                     category="VISIT"/>}/>
+                    <Route path="/playdate"
+                           element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks}
+                                                     category="PLAYDATE"/>}/>
+                    <Route path="/appointment"
+                           element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks}
+                                                     category="APPOINTMENT"/>}/>
+                    <Route path="/shopping"
+                           element={<TaskCardGallery allTasks={tasksList} getAllTasks={getAllTasks}
+                                                     category="SHOPPING"/>}/>
 
-            <Route path="tasks/:id" element={<DetailsTaskCard allTasks={tasksList}/>}/>
-            <Route path="new" element={<NewTaskCard/>}/>
-            <Route path="edit/:id" element={<EditTaskCard taskModels={tasksList}/>}/>
-        </Routes>
-    </div>
-  );
+                    <Route path="tasks/:id" element={<DetailsTaskCard allTasks={tasksList}/>}/>
+                    <Route path="new" element={<NewTaskCard/>}/>
+                    <Route path="edit/:id" element={<EditTaskCard taskModels={tasksList}/>}/>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
