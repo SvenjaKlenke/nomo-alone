dfa9db379df32f5e7bf5879900b595ae81ad3a44
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 17:19:34 +0200

    ada Logout Button and make some things responsive

--- frontend/src/element/Header.tsx
+++ frontend/src/element/Header.tsx
@@ -4,12 +4,19 @@ import './Header.css';
 import {NavLink} from "react-router-dom";
 
 function Header() {
+    function logoutUser() {
+
+    }
+
     return (
         <div>
             <header className="App-header">
                 <NavLink to="/" className="LinkHeader">
                     <img src={logo} className="App-logo" alt="logo"/>
                 </NavLink>
+                <div className="ButtonContainerHeader">
+                    <button className="RoundButtonHeader" onClick={logoutUser}>Logout</button>
+                </div>
             </header>
         </div>
     );
