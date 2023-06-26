23ea46e9208c4c0e462edeb63a12b97a54ccd5f8
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 13:59:15 +0200

    add ControllerUser and security in pom

---
+++ backend/src/main/java/de/neuefische/backend/security/ControllerUser.java
@@ -0,0 +1,23 @@
+package de.neuefische.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class ControllerUser {
+
+    @GetMapping("/me")
+    public String getUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @PostMapping("/login")
+    public void login() {
+    }
+}
