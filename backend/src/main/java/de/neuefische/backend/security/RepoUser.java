e296d65ceaddb47df2485a0959c157028fb0ca1f
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 14:08:47 +0200

    update RepoUser

--- backend/src/main/java/de/neuefische/backend/security/RepoUser.java
+++ backend/src/main/java/de/neuefische/backend/security/RepoUser.java
@@ -1,8 +1,11 @@
 package de.neuefische.backend.security;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class RepoUser extends MongoRepository<UserModel, String> {
+public interface RepoUser extends MongoRepository<UserModel, String> {
     Optional<UserModel> findUserByUsername(String username);
 }
