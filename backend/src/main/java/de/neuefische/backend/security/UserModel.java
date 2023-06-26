6a0110aa1058873422c3dbb36122cadff36cb07b
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 14:06:51 +0200

    add UserModel

---
+++ backend/src/main/java/de/neuefische/backend/security/UserModel.java
@@ -0,0 +1,19 @@
+package de.neuefische.backend.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("User")
+public class UserModel {
+
+    private String id;
+    private String username;
+    private String password;
+
+
+}
