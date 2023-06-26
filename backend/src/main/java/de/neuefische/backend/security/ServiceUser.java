4552539705a941a184edd2c2c545c06733bd3fbb
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 14:09:49 +0200

    import some security things in ServiceUser

--- backend/src/main/java/de/neuefische/backend/security/ServiceUser.java
+++ backend/src/main/java/de/neuefische/backend/security/ServiceUser.java
@@ -1,10 +1,13 @@
 package de.neuefische.backend.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
