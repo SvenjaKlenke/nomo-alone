1203fad0a911b3678dddcca4f648fa8e53483f46
svenjaklenke <svenja.klenke@gmx.de>
2023-06-26 14:02:49 +0200

    add ServiceUser

---
+++ backend/src/main/java/de/neuefische/backend/security/ServiceUser.java
@@ -0,0 +1,20 @@
+package de.neuefische.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ServiceUser implements UserDetailsService {
+    private final RepoUser repoUser;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = repoUser.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+        return new User(userModel.getUsername(), userModel.getPassword(), List.of());
+    }
+}
