899008f62e79e116e0f310a014d330853247b21f
svenjaklenke <svenja.klenke@gmx.de>
2023-06-27 15:51:15 +0200

    add UserModelDTO

---
+++ backend/src/main/java/de/neuefische/backend/security/UserModelDTO.java
@@ -0,0 +1,11 @@
+package de.neuefische.backend.security;
+
+public class UserModelDTO {
+
+    private String id;
+    private String username;
+    private String name;
+    private String lastname;
+    private String email;
+
+}
