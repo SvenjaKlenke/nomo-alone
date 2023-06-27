377989ddcabe6159f05df97cac573dfdb9053f56
svenjaklenke <svenja.klenke@gmx.de>
2023-06-27 17:53:36 +0200

    delete id by DTO

--- backend/src/main/java/de/neuefische/backend/security/UserModelDTO.java
+++ backend/src/main/java/de/neuefische/backend/security/UserModelDTO.java
@@ -3,19 +3,14 @@ package de.neuefische.backend.security;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document("User")
 public class UserModelDTO {
 
     private String username;
     private String name;
     private String lastname;
     private String email;
-    private String password;
-
-
 }
