4c82fa03e356ac0359104f02f5eadb6851baf20e
svenjaklenke <svenja.klenke@gmx.de>
2023-06-29 17:47:29 +0200

    add Spring Validation and update UserModelRequest

--- backend/src/main/java/de/neuefische/backend/security/UserModelRequest.java
+++ backend/src/main/java/de/neuefische/backend/security/UserModelRequest.java
@@ -1,5 +1,9 @@
 package de.neuefische.backend.security;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +13,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserModelRequest {
 
+    @NotEmpty
+    @Size(min = 4, max = 20)
     private String username;
+    @NotEmpty
+    @Pattern(regexp = "^[^0-9]*$")
     private String name;
+    @NotEmpty
     private String lastname;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
     private String password;
 }
