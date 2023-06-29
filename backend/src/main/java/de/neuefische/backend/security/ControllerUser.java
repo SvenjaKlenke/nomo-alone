d1c6fd0d560beedd6d3aa877c8f271c10ef0f0b2
svenjaklenke <svenja.klenke@gmx.de>
2023-06-29 18:22:28 +0200

    add Spring Validation in 'Controller'

--- backend/src/main/java/de/neuefische/backend/security/ControllerUser.java
+++ backend/src/main/java/de/neuefische/backend/security/ControllerUser.java
@@ -1,9 +1,15 @@
 package de.neuefische.backend.security;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
@@ -29,7 +35,20 @@ public class ControllerUser {
     }
 
     @PostMapping("/register")
-    public UserModelDTO addNewUser(@RequestBody UserModelRequest userModelRequest) {
+    public UserModelDTO addNewUser(@Valid @RequestBody UserModelRequest userModelRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError error : fieldErrors) {
+                if (error.getField().equals("email") && Objects.equals(error.getCode(), "email")) {
+                    String errorMessage = "Email address does not have a valid format.";
+                    bindingResult.rejectValue("email", error.getCode(), errorMessage);
+                }
+                if (error.getField().equals("password") && Objects.equals(error.getCode(), "password")) {
+                    String errorMessage = "Password must have at least 8 characters, including uppercase and lowercase letters, at least one number, and one special character.";
+                    bindingResult.rejectValue("password", error.getCode(), errorMessage);
+                }
+            }
+        }
         return serviceUser.addNewUser(userModelRequest);
     }
 
