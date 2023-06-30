e2cb7c9bbd87d6651f0efcf6c443ca9bec627f70
svenjaklenke <svenja.klenke@gmx.de>
2023-06-30 14:44:44 +0200

    add test and change addNewUserFunction

--- backend/src/main/java/de/neuefische/backend/security/ControllerUser.java
+++ backend/src/main/java/de/neuefische/backend/security/ControllerUser.java
@@ -4,15 +4,9 @@ package de.neuefische.backend.security;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
@@ -38,34 +32,7 @@ public class ControllerUser {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> addNewUser(@Valid @RequestBody UserModelRequest userModelRequest, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-            List<String> errorMessages = new ArrayList<>();
-
-            for (FieldError error : fieldErrors) {
-                String field = error.getField();
-                String errorCode = error.getCode();
-
-                if (Objects.equals(field, "email") && Objects.equals(errorCode, "email")) {
-                    String errorMessage = "Email address does not have a valid format.";
-                    bindingResult.rejectValue(field, errorCode, errorMessage);
-                    errorMessages.add(errorMessage);
-                }
-
-                if (Objects.equals(field, "password") && Objects.equals(errorCode, "password")) {
-                    String errorMessage = "Password must have at least 8 characters, including uppercase and lowercase letters, at least one number, and one special character.";
-                    bindingResult.rejectValue(field, errorCode, errorMessage);
-                    errorMessages.add(errorMessage);
-                }
-            }
-
-            if (!errorMessages.isEmpty()) {
-                return ResponseEntity.badRequest().body(errorMessages);
-            }
-        }
-
-        UserModelDTO userModelDTO = serviceUser.addNewUser(userModelRequest);
-        return ResponseEntity.ok(userModelDTO);
+    public UserModelDTO addNewUser(@Valid @RequestBody UserModelRequest userModelRequest) {
+        return serviceUser.addNewUser(userModelRequest);
     }
 }
---
+++ backend/src/test/java/de/neuefische/backend/security/ControllerUserTest.java
@@ -0,0 +1,40 @@
+package de.neuefische.backend.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ControllerUserTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void addNewUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                                                            
+                                                  "username": "Svenja",
+                                                  "name":"Test1",
+                                                  "lastname":"Test",
+                                                  "email": "test",
+                                                  "passwort": "123"
+                                       
+                                }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+}
