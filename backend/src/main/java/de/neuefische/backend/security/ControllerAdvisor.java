6435cf7ccda32c627d503b0e89adfd0ff8f66830
svenjaklenke <svenja.klenke@gmx.de>
2023-06-30 17:07:24 +0200

    change return

--- backend/src/main/java/de/neuefische/backend/security/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/backend/security/ControllerAdvisor.java
@@ -1,11 +1,10 @@
 package de.neuefische.backend.security;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,17 +12,17 @@ import java.util.Map;
 @ControllerAdvice
 public class ControllerAdvisor {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = getErrorMessage(fieldName);
             errors.put(fieldName, errorMessage);
         });
-        return errors;
+        return ResponseEntity.badRequest().body(errors);
     }
+
 
     private String getErrorMessage(String fieldName) {
         if (fieldName.equals("username")) {
