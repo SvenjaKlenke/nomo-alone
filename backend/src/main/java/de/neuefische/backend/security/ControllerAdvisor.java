723977848f2916e397b27c7f13804d44670270ed
svenjaklenke <svenja.klenke@gmx.de>
2023-06-30 17:43:01 +0200

    delete parentheses

--- backend/src/main/java/de/neuefische/backend/security/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/backend/security/ControllerAdvisor.java
@@ -15,7 +15,7 @@ public class ControllerAdvisor {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = getErrorMessage(fieldName);
             errors.put(fieldName, errorMessage);
