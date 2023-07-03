c435bfc3e6fcb78cfb74741ca2b2314bcc969fc9
svenjaklenke <svenja.klenke@gmx.de>
2023-07-03 12:36:00 +0200

    change SecurityConfig

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/user/**").permitAll();
                     auth.requestMatchers("/tasks/**").authenticated();
-                    auth.anyRequest().permitAll();
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
                 })
                 .build();
     }
