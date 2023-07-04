2f5e3f75def4ef249185bf723374124ff86cc6c5
svenjaklenke <svenja.klenke@gmx.de>
2023-07-04 16:21:30 +0200

    add show Password Button to LoginPage

--- frontend/src/login/LoginPage.tsx
+++ frontend/src/login/LoginPage.tsx
@@ -4,17 +4,17 @@ import logo from "../Logo.png";
 import {Link} from "react-router-dom";
 
 type Props = {
-    login: (username: string, password: string) => void
-}
+    login: (username: string, password: string) => void;
+};
 
 function LoginPage(props: Props) {
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     function loginOnSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        props.login(username, password)
+        props.login(username, password);
     }
 
     function onChangeHandlerUsername(e: ChangeEvent<HTMLInputElement>) {
@@ -25,24 +25,51 @@ function LoginPage(props: Props) {
         setPassword(e.target.value);
     }
 
+    function toggleShowPassword() {
+        setShowPassword(!showPassword);
+    }
+
     return (
         <div>
             <div className="LoginLogo">
                 <img src={logo} alt="Logo"/>
             </div>
-            <form className="form" onSubmit={loginOnSubmit}>
-                <div className="flex">
-                    <div className="login color">Login</div>
-                    <label className="color">Username :</label>
-                    <input type="text" className="input" onChange={onChangeHandlerUsername}/>
-                    <label className="color">Password :</label>
-                    <input type="password" className="input" onChange={onChangeHandlerPassword}/>
-                    <button className="">Login</button>
-                    <br/>
-                    <div className="color align">Don't have an account? <Link className="LinkRegister" to={`/register`}>Sign
-                        in</Link></div>
-                </div>
-            </form>
+            <div className="form-container">
+                <form className="form" onSubmit={loginOnSubmit}>
+                    <div className="flex">
+                        <div className="login color">Login</div>
+                        <label className="color">Username :</label>
+                        <input
+                            type="text"
+                            className="input"
+                            onChange={onChangeHandlerUsername}
+                        />
+                        <label className="color">Password :</label>
+                        <div className="password-input">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                className="input password"
+                                onChange={onChangeHandlerPassword}
+                            />
+                            <button
+                                type="button"
+                                className="show-password-button"
+                                onClick={toggleShowPassword}
+                            >
+                                {showPassword ? "Hide" : "Show"}
+                            </button>
+                        </div>
+                        <button className="">Login</button>
+                        <br/>
+                        <div className="color align">
+                            Don't have an account?{" "}
+                            <Link className="LinkRegister" to={`/register`}>
+                                Sign in
+                            </Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
